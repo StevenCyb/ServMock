80d08cb5890bd4b23d11912f621b12e2aa02981d
Steven Cyb <41087154+StevenCyb@users.noreply.github.com>
2025-07-14 16:34:29 +0200

    refactor: change package name from parser to setup in multiple files

--- pkg/setup/utils.go
+++ pkg/setup/utils.go
@@ -1,4 +1,4 @@
-package parser
+package setup
 
 // Ptr is a utility function to create a pointer to a value of type T.
 func Ptr[T int | int8 | int16 | int32 | int64 | uint | uint8 | uint16 | uint32 | uint64 | float32 | float64 | string](v T) *T {
